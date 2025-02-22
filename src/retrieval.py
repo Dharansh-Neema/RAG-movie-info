c48b7e1943287938a54adc4ac70b1920ffeed71e
Dharansh Neema <dharanshneema@gmail.com>
2025-02-23 01:06:17 +0530

    embedding model on retrieval

--- src/retrieval.py
+++ src/retrieval.py
@@ -2,13 +2,36 @@ import openai
 from src.logger import logger
 import os
 from langchain_openai import ChatOpenAI
+from pinecone.grpc import PineconeGRPC as Pinecone
+from sentence_transformers import SentenceTransformer
+
 class Retrieval:
-    _OPEN_AI_KEY = os.getenv("OPENAI_API_KEY") 
     def __init__(self):
-        llm = ChatOpenAI(
+        self.__OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
+        self.__PINECONE_API = os.getenv("PINECONE_API") 
+        self.llm = ChatOpenAI(
                 model="gpt-3.5-turbo",
-                api_key=OPEN_AI_KEY,
+                api_key=self.__OPEN_AI_KEY,
                 temperature=0.7,         
                 max_tokens=500)   
-response = llm(prompt)
-print(response)
+
+        self.pc = Pinecone(api_key=self.__PINECONE_API)
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+    def _get_index(self):
+        try:
+            index = self.pc("https://embeddings-pdf-index-gfb3fgs.svc.aped-4627-b74a.pinecone.io")
+            logger.debug("Fetched index for retrieval")
+            return index
+        except Exception as e:
+            logger.error("Error occurred while fetching the pinecone index".e)
+            raise
+        
+    def get_context(self,query:str):
+        try:
+            index = self._get_index()
+
+        except Exception as e:
+            logger.error("Error occured while fethcing the context",e)
+
+        
+    
